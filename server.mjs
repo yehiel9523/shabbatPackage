15560fabdbf4e2cf8c77e6df6be71ede4945573a
yehiel <yehiel9523@gmail.com>
2021-08-15 04:05:19 +0300

    add options to the funtion and fix city

--- server.mjs
+++ server.mjs
@@ -10,13 +10,9 @@ import Hebcal from 'hebcal';
 // console.log(date().candleLighting())
 // console.log(now().getTime())
 
-export function shabbatChack(req, res, next) {
-    const date = () => new Hebcal.HDate(new Date());
-    const now = () => new Date();
-    date().setCity('Jerusalem');
-    if (test || date().candleLighting()) {
-        if (test || date().candleLighting().getTime() < now().getTime()) {
-            // app.use(express.static('/'))
+export function shabbatChack(test = false, city = 'Jerusalem') {
+    if (test)
+        return (req, res, next) => {
             res.set('Content-Type', 'text/html');
             res.send(`<!DOCTYPE html>
                 <html lang="en">
@@ -35,12 +31,44 @@ export function shabbatChack(req, res, next) {
                 </body>
     
                 </html>`)
-        } else next()
-    } else
-    if (date().havdalah()) {
-        if (date().havdalah().getTime() > now().getTime()) {
-            res.set('Content-Type', 'text/html');
-            res.send(`<!DOCTYPE html>
+        }
+    else {
+
+
+        return (req, res, next) => {
+            const date = () => {
+                const hebcalDate = new Hebcal.HDate(new Date())
+                hebcalDate.setCity(city);
+                return hebcalDate
+            };
+            const now = () => new Date();
+            if (date().candleLighting()) {
+                if (date().candleLighting().getTime() < now().getTime()) {
+                    // app.use(express.static('/'))
+                    res.set('Content-Type', 'text/html');
+                    res.send(`<!DOCTYPE html>
+                <html lang="en">
+    
+                <head>
+                    <meta charset="UTF-8">
+                    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+                    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                    <title>Shabbat Kodesh</title>
+                </head>
+    
+                <body>
+                    <h1 style="text-align: center;"> !!! שבת קודש</h1>
+                    <img style="width: 1500px;" src="https://www.kipa.co.il/userFiles/735-415/296656_373af8629a50ff540978724a33c9d96a.jpg" alt="shabbat kodesh">
+    
+                </body>
+    
+                </html>`)
+                } else next()
+            } else
+            if (date().havdalah()) {
+                if (date().havdalah().getTime() > now().getTime()) {
+                    res.set('Content-Type', 'text/html');
+                    res.send(`<!DOCTYPE html>
                     <html lang="en">
     
                     <head>
@@ -58,11 +86,13 @@ export function shabbatChack(req, res, next) {
     
                     </html>`)
 
-        } else next()
-    } else {
-        next()
-    }
+                } else next()
+            } else {
+                next()
+            }
 
+        }
+    }
 }
 
 const app = express();
